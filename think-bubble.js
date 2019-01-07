a38a94b8034a3dadf62199f3c7a1571bd1522e36
Henry Darnell <hjdarnel@users.noreply.github.com>
2019-01-07 16:23:46 -0600

    Add rest

--- think-bubble.js
+++ think-bubble.js
@@ -4,23 +4,14 @@ module.exports = (req, res) => {
     const { query } = parse(req.url, true);
     const { text = "Use query 'text'" } = query;
     const [ who, what, ...rest] = text.split(' ');
-    if (rest.length > 0) {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({
-            response_type: "ephemeral",
-            text: `Only pass 2 options: who and what`
-        }));
-    } else {
-
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({
-            response_type: "in_channel",
-            text: `
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+        response_type: "in_channel",
+        text: `
 :blank::thought-balloon-top-left::thought-balloon-top-middle::thought-balloon-top-right:
 :blank::thought-balloon-center-left:${what}:thought-balloon-center-right:
 :blank::thought-balloon-bottom-left::thought-balloon-bottom-middle::thought-balloon-bottom-right:
-${who}
-            `
-        }));
-    }
+${who}${rest.join(' ')}
+        `
+    }));
 };
