7da94a229372b8cd08c5e9fec2823af83e968ff2
Henry Darnell <hjdarnel@users.noreply.github.com>
2019-02-06 19:25:48 -0600

    Add k8s and some dev stuff

---
+++ k8s-speak.js
@@ -0,0 +1,47 @@
+const { parse } = require("url");
+
+const alphanumerize = (tokens) => {
+    return tokens.map((word) => word.replace(/[^0-9a-zA-Z\.\!\?]/g, ""));
+};
+
+const k8ize = (tokens) => {
+    return tokens.map((word) => {
+        let punctuation = "";
+
+        const replaced = word.replace(/[\!\?\.]/g, (match) => {
+            punctuation = `${punctuation}${match}`;
+            return ""
+        });
+
+        const length = replaced.length - 2;
+        console.log(replaced);
+
+        if (length > 1) {
+            const first = replaced.slice(0, 1);
+            const last = replaced.slice(-1);
+            return `${first}${length}${last}${punctuation}`;
+        } else {
+            return `${replaced}${punctuation}`;
+        }
+    })
+};
+
+module.exports = (req, res) => {
+    const { query } = parse(req.url, true);
+    let text = "Pass in a value!";;
+
+    if (query.text) {
+        const tokens = query.text.split(' ');
+
+        const stripped = alphanumerize(tokens);
+        const bareWords = k8ize(stripped);
+
+        text = bareWords.join(' ');
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+        response_type: "in_channel",
+        text
+    }));
+};
