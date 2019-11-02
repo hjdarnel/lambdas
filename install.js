624f575953482fd08e02ae5f3cb3095307bbbadc
Henry Darnell <hjdarnel@users.noreply.github.com>
2019-11-02 01:49:56 -0500

    Add OAuth setup

---
+++ install.js
@@ -0,0 +1,6 @@
+module.exports = async (req, res) => {
+    const slackUrl = `https://slack.com/oauth/authorize?client_id=${process.env.SLACK_CLIENT_ID}&scope=commands`;
+    // Send redirect response to slack domain
+    res.writeHead(302, 'Redirect', { Location: slackUrl })
+    res.end()
+}
